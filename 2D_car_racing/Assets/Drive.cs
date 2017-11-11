8ea43daa5f934c9f8e8c5775801913756a517ffe
ktsiam <ktsiampouris@gmail.com>
2017-11-11 15:01:45 -0500

    init unity project

---
+++ 2D_car_racing/Assets/Drive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public int count = 1;
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = (new Vector2 (0, 0));
+
+	}
+
+	void Update () {
+
+		float rot = transform.localEulerAngles.z ;
+		if (Input.GetKey ("a")) { 
+			transform.localEulerAngles = new Vector3 (0.0f, 0.0f, rot + 4.0f);
+		} else if (Input.GetKey ("d")) {
+			
+			transform.localEulerAngles = new Vector3 (0.0f, 0.0f, rot - 4.0f);
+		} 
+		rb.velocity = transform.up * 1;
+	}
+}
+
